24462bf8bc9e89c95f032981652724323989718e
DIMON710 <55649421+DIMON710@users.noreply.github.com>
2023-11-27 17:27:38 +0200

    Add tRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
+import { trpc } from "@/utils/trpc";
 
 export default function Home() {
+  const { data, isLoading } = trpc.hello.useQuery({ text: 'client' });
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (data) {
+    return <div>{data.greeting}</div>
+  }
   return (
-    <div className="text-red-200">Hello world</div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Wich Pokemon is Rounder?</div>
+      <div className="p-2"/>
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200"/>
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200"/>
+      </div>
+    </div>
   )
 }
