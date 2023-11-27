aad45c0d5084bb7cc685c8837af301201cecaa94
DIMON710 <55649421+DIMON710@users.noreply.github.com>
2023-11-27 18:12:32 +0200

    Add random pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,26 @@
-import { trpc } from "@/utils/trpc";
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 
-export default function Home() {
-  const { data, isLoading } = trpc.hello.useQuery({ text: 'client' });
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  if (data) {
-    return <div>{data.greeting}</div>
-  }
+export default function Home({ first, second }: { first: number, second: number }) {
+  
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Wich Pokemon is Rounder?</div>
       <div className="p-2"/>
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200"/>
+        {first && <div className="w-16 h-16 bg-red-800">{first}</div>}
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200"/>
+        {second && <div className="w-16 h-16 bg-red-800">{second}</div>}
       </div>
     </div>
   )
+}
+
+export const getServerSideProps = () => {
+  const [first, second] = getOptionsForVote();
+  return {
+    props: {
+      first,
+      second
+    }
+  }
 }
