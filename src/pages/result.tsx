d3ef158d5bde2508f985c662bcd32111d44c9fe7
DIMON710 <55649421+DIMON710@users.noreply.github.com>
2023-11-29 13:26:10 +0200

    fix deploy

---
+++ src/pages/result.tsx
@@ -0,0 +1,53 @@
+import { GetStaticProps } from "next";
+import React from "react";
+import prisma from "@/server/utils/prisma";
+
+const ResultPage: React.FC<{ pokemon }> = ({ pokemon }) => {
+    
+    return <div>result</div>;
+}
+
+export default ResultPage;
+
+const getPokemonInOrder = async () => {
+    return prisma.pokemon.findMany({
+        orderBy: {
+            voteFor: { _count: "desc" },
+        },
+        select: {
+            id: true,
+            name: true,
+            spriteUrl: true,
+            _count: {
+                select: { 
+                    voteAgainst: true, 
+                    voteFor: true, 
+                },
+            },
+        },
+    });
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const pokemonOrdered = await prisma.pokemon.findMany({
+        orderBy: {
+            voteFor: { _count: "desc" },
+        },
+        select: {
+            id: true,
+            name: true,
+            spriteUrl: true,
+            _count: {
+                select: { 
+                    voteAgainst: true, 
+                    voteFor: true, 
+                },
+            },
+        },
+    });
+    return {
+        props: {
+            pokemon: pokemonOrdered,
+        },
+    };
+};
